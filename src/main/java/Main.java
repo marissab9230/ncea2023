52d3b0815de3808a474f48d27d122cf0118d2a54
burnema <marissab9230@gmail.com>
2023-05-18 11:59:11 +1200

    started project

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,15 +3,26 @@ import java.util.Scanner;
 public class Main {
     static Scanner inputStream = new Scanner(System.in);
 
+    static int minNumberOfRounds = 5;
+    static int maxNumberOfRounds = 10;
+    static int numberOfRounds;
+    static int userScore;
+    static int computerScore;
+    static String[] userMoves;
+
+
     public static void main(String args[]){
     }
 
-    public static String promptedInput(String prompt){
-        System.out.println(prompt);
-        return inputStream.nextLine();
+    public static void startGame(){ //this starts the game
+        userScore = 0;
+        computerScore = 0;
+        numberOfRounds = (int) Math.floor(Math.random()*(maxNumberOfRounds - minNumberOfRounds + 1) + minNumberOfRounds);
+        userMoves = new String[numberOfRounds];
     }
 
-    public static void interpretInput(String input){
-        input= (input.toLowerCase());
+    public static String promptedInput(String prompt) { //this prints a prompt and takes in an input
+        System.out.println(prompt);
+        return inputStream.nextLine();
     }
 }
