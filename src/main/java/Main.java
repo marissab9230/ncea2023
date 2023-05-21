af8588ae908161f5732c54c7f07311397f0b64cb
burnema <marissab9230@gmail.com>
2023-05-22 09:41:44 +1200

    continued code for the general framework of the game,

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,7 +9,9 @@ public class Main {
     static int userScore;
     static int computerScore;
     static String[] userMoves;
-
+    static String gameInstructions; //description of the game and instructions , to-do
+    static String roundInstructions;//prints every round, asks user to defect/stick to story
+    static int roundNumber;
 
     public static void main(String args[]){
     }
@@ -19,10 +21,29 @@ public class Main {
         computerScore = 0;
         numberOfRounds = (int) Math.floor(Math.random()*(maxNumberOfRounds - minNumberOfRounds + 1) + minNumberOfRounds);
         userMoves = new String[numberOfRounds];
+        roundNumber=0;
+
+        System.out.println(gameInstructions);
+
     }
 
-    public static String promptedInput(String prompt) { //this prints a prompt and takes in an input
+    public static void round(){
+        userMoves[roundNumber]=promptedInput(roundInstructions);
+
+        roundNumber++;
+    }
+
+    public static void settings(){
+        //print settings options and allow user to adjust language, input options
+    }
+
+    public static String promptedInput(String prompt) { //this prints a prompt and takes in an input. TO-DO: MAKE INPUT RELIABLE
         System.out.println(prompt);
-        return inputStream.nextLine();
+        String input = inputStream.nextLine();
+        input=(input.toLowerCase());
+        input=
+        switch(input){
+
+        }
     }
 }
